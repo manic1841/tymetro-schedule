f71e8e6df996deb9f19d2c86723da3f7228f0098
manic1841 <manic35153@gmail.com>
2025-04-07 18:04:44 +0800

    fix: link with wrong js and css path

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.tymetro.com.tw',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' from the request path
-        headers: {
-          Referer: 'https://www.tymetro.com.tw/tymetro-new/tw/_pages/travel-guide/timetable-search.php',
-        },
-      },
-    },
-  },
+  base: '/tymetro-schedule/',
+  plugins: [react()]
 })
